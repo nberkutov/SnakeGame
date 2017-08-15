6c73e339a2f9f0ba244c9b88f49d899e4a5ee2dc
Nikita Berkutov <nick9733@gmail.com>
2017-08-15 19:17:03 +0600

    .

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,4 @@
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class Snake : MonoBehaviour {    public float speed = 10f;	public void Move()    {    }    public void Turn()    {    }    public void IncreaseTail()
+    {
+
+    }}
