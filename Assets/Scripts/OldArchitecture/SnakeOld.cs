c670aa49a05cd4d4ac9fc3061218d31ed3ec837a
Nikita Berkutov <nick9733@gmail.com>
2017-08-15 19:19:06 +0600

    debug

--- Assets/Scripts/OldArchitecture/SnakeOld.cs
+++ Assets/Scripts/OldArchitecture/SnakeOld.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Snake : MonoBehaviour
+public class SnakeOld : MonoBehaviour
 {
 
     public int maxLength = 10;
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,4 +1,23 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class Snake : MonoBehaviour {    public float speed = 10f;	public void Move()    {    }    public void Turn()    {    }    public void IncreaseTail()
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+
+    public float speed = 10f;
+
+	public void Move()
     {
 
-    }}
+    }
+
+    public void Turn()
+    {
+
+    }
+
+    public void IncreaseTail()
+    {
+
+    }
+}
